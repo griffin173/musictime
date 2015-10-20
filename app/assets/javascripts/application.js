e497dc986731857f0b0767f4d918a16a25bdb39b
jeff <jeff@mediamiser.com>
2015-10-20 14:55:04 -0400

    remove unused gems, comment code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,19 +15,20 @@
 //= require bootstrap-sprockets
 //= require underscore
 //= require gmaps/google
-//= require turbolinks
 //= require_tree .
 var initialLocation;
-var siberia = new google.maps.LatLng(60, 105);
-var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-var browserSupportFlag =  new Boolean();
+var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);//if people don't share location put them in new york
+
 var map;
 var geocoder;
-var infowindow;
 var placesApi;
-var markers = new Object();
+var selectedGig;
+var markers = new Object(); // array of map markers to bounce/center on when user clicks icon in result list.
 $( document ).ready(function() {
+selectedGig=0;
 initialize();
+
+//put the search bar on the map
 var input = document.getElementById('map-search');
 var searchBox = new google.maps.places.SearchBox(input);
 map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
@@ -36,80 +37,84 @@ map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 map.addListener('bounds_changed', function() {
   searchBox.setBounds(map.getBounds());
 });
+//when the search box has it's value changed, update the listings
 searchBox.addListener('places_changed', function() {
 
-  $( "#results-container"  ).fadeTo( "slow" , 0.3)
-  $( "#results-container" ).html('');
   geocoder.geocode({'address': $( "#map-search" ).val()}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
+
       map.setCenter(results[0].geometry.location);
-      $.ajax({
-        url: "/ajax",
-        data: {
-          lat: map.getCenter().lat(),
-          lng: map.getCenter().lng()
-        }
-      })
-        .done(function( data ) {
-          $( "#results-container" ).html(data)
-          bindButtons();
-          addMarkerListeners(data)
-          $( "#results-container"  ).fadeTo( "slow" , 1)
-        });
+      getListings()
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
   });
-
-
-
 });
+
+//implement infinite scroll, load more when you scroll to bottom
 $(window).on('scroll', function() {
-        if($(window).scrollTop() == $(document).height() - $(window).height()) {
-          $( "#results-container"  ).fadeTo( "slow" , 0.3)
-          $.ajax({
-            url: "/loadmore",
-            data: {
-              metro_id: $('#metro-id').attr('data-metro'),
-              page: $('#page').attr('data-page-number'),
-              start_day: $(".seperator" ).last().text()
-            }
-          })
-            .done(function( data ) {
-              var pageNumber = $('#page').attr('data-page-number')
-              pageNumber++
-              $('#page').attr('data-page-number', pageNumber)
-              $( "#results-container" ).append(data)
-
-              addMarkerListeners(data)
-              bindButtons()
-              $( "#results-container"  ).fadeTo( "slow" , 1)
-            });
-        }
-    });
-
+  if($(window).scrollTop() == $(document).height() - $(window).height()) {
+    loadmore();
+  }
+});
 
 bindButtons()
  
 
- // map.addListener('dragend', function() {
- //    geocoder.geocode({'location': map.getCenter()}, function(results, status) {
- //    if (status === google.maps.GeocoderStatus.OK) {
- //      if (results[1]) {
- //        $( "#address-box" ).val(results[1].formatted_address);
- //      } else {
- //        window.alert('No results found');
- //      }
- //    } else {
- //      window.alert('Geocoder failed due to: ' + status);
- //    }
- //  });
- //  });
-
+//position the music player
 window.addEventListener('resize', positionElements);
 positionElements();
 
 });
+
+//load more results into the results container, appending them at the bottom. 
+function loadmore() {
+  //apply a fading animation while loading
+  $( "#results-container"  ).fadeTo( "slow" , 0.3)
+  $.ajax({
+    url: "/loadmore",
+    data: {
+      metro_id: $('#metro-id').attr('data-metro'),
+      page: $('#page').attr('data-page-number'),
+      start_day: $(".seperator" ).last().text()
+    }
+  })
+    .done(function( data ) {
+      var pageNumber = $('#page').attr('data-page-number')
+      pageNumber++
+      $('#page').attr('data-page-number', pageNumber)
+      $( "#results-container" ).append(data)
+
+      addMarkerListeners(data)
+      bindButtons()
+      $( "#results-container"  ).fadeTo( "slow" , 1)
+    });
+}
+
+//load in initial list of results
+function getListings() {
+
+  $( "#results-container"  ).fadeTo( "slow" , 0.3)
+  $( "#results-container" ).html('');
+  $.ajax({
+    url: "/ajax",
+    data: {
+      lat: map.getCenter().lat(),
+      lng: map.getCenter().lng()
+    }
+  })
+    .done(function( data ) {
+      $( "#results-container" ).html(data)
+      bindButtons();
+      addMarkerListeners(data)
+      $( "#results-container"  ).fadeTo( "slow" , 1)
+    });
+}
+
+//place the markers on the map and apply listeners on the results list divs, so that markers appear
+//or dissappear based on what results are showing on the list
+//html is the string representation of the html that is appended during a loadmore, we want to just check through
+//this instead of reapplying to markers that already exist.
 function addMarkerListeners(html) {
   if (typeof(html) != "undefined" && html !== null) {
     html = $.parseHTML( html )
@@ -119,32 +124,35 @@ function addMarkerListeners(html) {
     gigs = $( ".gigResult" )
   }
   gigs.each(function( index ) {
-    var test = $(this).find(".venue");
+    var venue = $(this).find(".venue");
     var gigId = ($(this).attr("id"));
+    //apply the scroll monitoring ability to each result div
     var myElement = document.getElementById($(this).attr("id"));
-
     var elementWatcher = scrollMonitor.create( myElement );
 
     elementWatcher.enterViewport(function() {
       if (gigId in markers) {
-          markers[gigId].setMap(map)
+        // if marker exists already make it visible
+        markers[gigId].setMap(map)
       } else {
-
-        if (test.attr("data-lat")) {
-
+        // create marker with given lat and long
+        if (venue.attr("data-lat")) {
           var address;
-          address = {lat: Number(test.attr("data-lat")), lng: Number(test.attr("data-lng"))}
+          address = {lat: Number(venue.attr("data-lat")), lng: Number(venue.attr("data-lng"))}
           var marker = new google.maps.Marker({
             position: address,
             map: map
           });
           markers[gigId] = marker;
+          marker.addListener('click', function() {
+            $( ".gigResult#"+selectedGig ).removeClass("gig-selected")
+            selectedGig = gigId;
+            $( ".gigResult#"+gigId ).addClass("gig-selected")
+          });
         } else {
-
+          //if lat and long not provided try to find address from google places api based on venue name
           var address;
-
-  
-          placesApi.nearbySearch({location: map.getCenter(), name: test.attr("data-venue-name"), radius: 25000}, function(results, status) {
+          placesApi.nearbySearch({location: map.getCenter(), name: venue.attr("data-venue-name"), radius: 25000}, function(results, status) {
             if (status === google.maps.places.PlacesServiceStatus.OK) {
               address = {lat: Number(results[0].geometry.location.lat()), lng: Number(results[0].geometry.location.lng())}
               var marker = new google.maps.Marker({
@@ -152,6 +160,12 @@ function addMarkerListeners(html) {
                 map: map
               });
               markers[gigId] = marker;
+              marker.addListener('click', function() {
+
+                $( ".gigResult#"+selectedGig ).removeClass("gig-selected")
+                selectedGig = gigId;
+                $( ".gigResult#"+gigId ).addClass("gig-selected")
+              });
             } else {
             }
           });
@@ -161,6 +175,7 @@ function addMarkerListeners(html) {
       }
     });
     elementWatcher.exitViewport(function() {
+      //hide marker as result exits view
       if (gigId in markers) 
         markers[gigId].setMap(null)
     });
@@ -172,44 +187,48 @@ function positionElements() {
     $('.player').first().css('top', (window.innerHeight - 445)/2);
     $('.player').first().css('left', ($('#map-container').width() - 340)/2);
 }
-
+//bind the buttons for each result
 function bindButtons() {
   $('[data-toggle="tooltip"]').tooltip();
+  //remove any current bindings
   $( ".spotify-list" ).off( "click", "**" );
   $( ".youtube-search" ).off( "click", "**" );
-  	$( ".spotify-list" ).click(function() {
-    event.stopPropagation()
-	  event.preventDefault()
-	      $.ajax({
-	        url: "/songlist",
-	        data: {
-	          songkick_show_id: $(this).attr('data-songkick-id')
-	        }
-	      })
-	        .done(function( data ) {
-              var widget = $('#youtube-widget');
-              widget.html(data);
-              widget.parent('.player').addClass('visible');
-	        });
-	   
-	  });
-    $( ".youtube-search" ).click(function() {
+  //load in a spotify playlist for the concert
+	$( ".spotify-list" ).click(function() {
     event.stopPropagation()
     event.preventDefault()
-        $.ajax({
-          url: "/youtubesearch",
-          data: {
-            songkick_band_id: $(this).attr('data-songkick-band-id')
-          }
-        })
-          .done(function( data ) {
-              var widget = $('#youtube-widget');
-              widget.html(data);
-              widget.parent('.player').addClass('visible');
-          });
-     
-    });
-    $( ".map-marker" ).click(function() {
+    $.ajax({
+      url: "/songlist",
+      data: {
+        songkick_show_id: $(this).attr('data-songkick-id')
+      }
+    })
+      .done(function( data ) {
+        var widget = $('#youtube-widget');
+        widget.html(data);
+        widget.parent('.player').addClass('visible');
+      });
+	   
+	  });
+  // load in a youtube window for the band
+  $( ".youtube-search" ).click(function() {
+  event.stopPropagation()
+  event.preventDefault()
+      $.ajax({
+        url: "/youtubesearch",
+        data: {
+          songkick_band_id: $(this).attr('data-songkick-band-id')
+        }
+      })
+        .done(function( data ) {
+            var widget = $('#youtube-widget');
+            widget.html(data);
+            widget.parent('.player').addClass('visible');
+        });
+   
+  });
+  //clicking on a map marker icon in the results centers on the marker on the map and bounces
+  $( ".map-marker" ).click(function() {
     event.stopPropagation()
     event.preventDefault()
     marker = markers[$(this).attr("data-songkick-id")]
@@ -220,12 +239,13 @@ function bindButtons() {
         marker.setAnimation(null)
     }, 2220);
      
-    });
+  });
 }
 
-
+//initialize the google maps and location and load initial results
 function initialize() {
   var draggable = true;
+  //disable map drag on touch devices or you can get stuck on the map by zooming in
   if (Modernizr.touchevents) 
     draggable=false;
   var myOptions = {
@@ -233,48 +253,29 @@ function initialize() {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     draggable: draggable
   };
+  //load google maps and services
   map = new google.maps.Map(document.getElementById("map"), myOptions);
 
   placesApi = new google.maps.places.PlacesService(map);
   geocoder = new google.maps.Geocoder;
-  infowindow = new google.maps.InfoWindow;
   // Try W3C Geolocation (Preferred)
   if(navigator.geolocation) {
-    browserSupportFlag = true;
     navigator.geolocation.getCurrentPosition(function(position) {
       initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
       map.setCenter(initialLocation);
-      $( "#results-container"  ).fadeTo( "slow" , 0.3)
-      $.ajax({
-        url: "/ajax",
-        data: {
-          lat: map.getCenter().lat(),
-          lng: map.getCenter().lng()
-        }
-      })
-        .done(function( data ) {
-          $( "#results-container" ).html(data)
-          bindButtons();
-          addMarkerListeners(data)
-
-          $( "#results-container"  ).fadeTo( "slow" , 1)
-        });
+      getListings();
     }, function() {
-      handleNoGeolocation(browserSupportFlag);
+      handleNoGeolocation();
     });
   }
   // Browser doesn't support Geolocation
   else {
-    browserSupportFlag = false;
-    handleNoGeolocation(browserSupportFlag);
+    handleNoGeolocation();
   }
-
-  function handleNoGeolocation(errorFlag) {
-    if (errorFlag == true) {
-      initialLocation = newyork;
-    } else {
-      initialLocation = siberia;
-    }
+  //if we don't know where they are put them in new york
+  function handleNoGeolocation() {
+    initialLocation = newyork;
     map.setCenter(initialLocation);
+    getListings();
   }
 }
